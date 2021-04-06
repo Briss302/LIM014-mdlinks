afa5971d0d09e179af51922675246a403ace5300
Briceyda <Brd0208@gmail.com>
2021-04-06 10:33:28 -0600

    Comentarios de descripción de la función md-links

--- src/md-links.js
+++ src/md-links.js
@@ -1,11 +1,16 @@
+const { isAbsolute } = require('path');
 const path = require('path');
 const { pathResolve } = require('./index')
 // Aqui me retorna una promesa
+// opcion = {validate: true o false} => trabajar en esta funcion
+// resolve y reject en funcion mdlinks
 const mdLinks = ((route, opcion) => {
+
   const normalizePath = path.normalize(route);
   return pathResolve(normalizePath)
+})
 
-});
+
 
 module.exports = {
   mdLinks,
